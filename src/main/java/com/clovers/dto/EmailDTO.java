d7f53c4a4c14d859b3923d021d55189fe39584cd
anuygnaj <anuygnaj@naver.com>
2023-11-06 20:11:52 +0900

    lombok added

--- src/main/java/com/clovers/dto/EmailDTO.java
+++ src/main/java/com/clovers/dto/EmailDTO.java
@@ -2,6 +2,17 @@ package com.clovers.dto;
 
 import java.sql.Timestamp;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class EmailDTO {
 	private int id;
 	private String send_id;
@@ -12,95 +23,4 @@ public class EmailDTO {
 	private boolean reservation;
 	private Timestamp reservation_date;
 	private boolean trash;
-	
-	public EmailDTO() {}
-	
-	public EmailDTO(int id, String send_id, String receive_id, String title, String content, boolean temporary,
-			boolean reservation, Timestamp reservation_date, boolean trash) {
-		super();
-		this.id = id;
-		this.send_id = send_id;
-		this.receive_id = receive_id;
-		this.title = title;
-		this.content = content;
-		this.temporary = temporary;
-		this.reservation = reservation;
-		this.reservation_date = reservation_date;
-		this.trash = trash;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	public String getSend_id() {
-		return send_id;
-	}
-	
-	public void setSend_id(String send_id) {
-		this.send_id = send_id;
-	}
-	
-	public String getReceive_id() {
-		return receive_id;
-	}
-	
-	public void setReceive_id(String receive_id) {
-		this.receive_id = receive_id;
-	}
-	
-	public String getTitle() {
-		return title;
-	}
-	
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	
-	public String getContent() {
-		return content;
-	}
-	
-	public void setContent(String content) {
-		this.content = content;
-	}
-	
-	public boolean isTemporary() {
-		return temporary;
-	}
-	
-	public void setTemporary(boolean temporary) {
-		this.temporary = temporary;
-	}
-	
-	public boolean isReservation() {
-		return reservation;
-	}
-	
-	public void setReservation(boolean reservation) {
-		this.reservation = reservation;
-	}
-	
-	public Timestamp getReservation_date() {
-		return reservation_date;
-	}
-	
-	public void setReservation_date(Timestamp reservation_date) {
-		this.reservation_date = reservation_date;
-	}
-	
-	public boolean isTrash() {
-		return trash;
-	}
-	
-	public void setTrash(boolean trash) {
-		this.trash = trash;
-	}
-	
-	
-	
 }
--- src/main/java/com/clovers/dto/EmailFileDTO.java
+++ src/main/java/com/clovers/dto/EmailFileDTO.java
@@ -1,50 +1,21 @@
 package com.clovers.dto;
 
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class EmailFileDTO {
 	private int id;
 	private int email_id;
 	private String ori_name;
 	private String sys_name;
-	
-	public EmailFileDTO() {}
-	
-	public EmailFileDTO(int id, int email_id, String ori_name, String sys_name) {
-		super();
-		this.id = id;
-		this.email_id = email_id;
-		this.ori_name = ori_name;
-		this.sys_name = sys_name;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public int getEmail_id() {
-		return email_id;
-	}
-
-	public void setEmail_id(int email_id) {
-		this.email_id = email_id;
-	}
-
-	public String getOri_name() {
-		return ori_name;
-	}
-
-	public void setOri_name(String ori_name) {
-		this.ori_name = ori_name;
-	}
-
-	public String getSys_name() {
-		return sys_name;
-	}
-
-	public void setSys_name(String sys_name) {
-		this.sys_name = sys_name;
-	}
 }
