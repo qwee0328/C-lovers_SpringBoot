f4585d39faa791cb7ac0b4e295b008a9a9a0ddfd
HMK <cypher_1013@naver.com>
2023-11-06 10:33:57 +0900

    member login.css, login.jsp added

--- src/main/java/com/clovers/controllers/MemberController.java
+++ src/main/java/com/clovers/controllers/MemberController.java
@@ -6,5 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/members/")
 public class MemberController {
+	// 멤버 로그인, 비밀번호 확인
 	
+	@RequestMapping("goLogin")
+	public String goLogin() {
+		return "member/login";
+	}
 }
