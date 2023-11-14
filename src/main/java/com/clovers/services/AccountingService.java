6a22df30acc4dd601a713bb11bdd760ccc5e4b90
HMK <cypher_1013@naver.com>
2023-11-14 09:05:18 +0900

    직원계좌리스트 출력

--- src/main/java/com/clovers/services/AccountingService.java
+++ src/main/java/com/clovers/services/AccountingService.java
@@ -1,8 +1,21 @@
 package com.clovers.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.clovers.dao.AccountingDAO;
+import com.clovers.dto.AccountingDTO;
 
 @Service
 public class AccountingService {
 	// 회계지원 서비스 레이어
+	
+	@Autowired
+	private AccountingDAO adao;
+	
+	public List<AccountingDTO> selectAccountAll(){
+		return adao.selectAccountAll();
+	}
 }
