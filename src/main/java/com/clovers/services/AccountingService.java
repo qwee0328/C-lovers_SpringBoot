13537edf006b7f9c698ac893782fe6025b5e9b25
HMK <cypher_1013@naver.com>
2023-11-16 15:52:37 +0900

    직원법인카드 CRUD, 검색, 페이지네이션 완료

--- src/main/java/com/clovers/services/AccountingService.java
+++ src/main/java/com/clovers/services/AccountingService.java
@@ -1,9 +1,13 @@
 package com.clovers.services;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.clovers.dao.AccountingDAO;
 import com.clovers.dto.AccountingDTO;
@@ -17,5 +21,38 @@ public class AccountingService {
 	
 	public List<AccountingDTO> selectAccountAll(){
 		return adao.selectAccountAll();
+	}
+	
+	public List<AccountingDTO> searchBy(String keyword){
+		return adao.searchBy(keyword);
+	}
+	
+	public int insert( AccountingDTO dto){
+		return adao.insert(dto);
+	}
+	
+	public int deleteAccount(String id){
+		return adao.deleteAccount(id);
+	}
+	
+	public int update(AccountingDTO dto){
+		return adao.update(dto);
+	}
+	
+	/////////
+	public List<AccountingDTO> selectCardAll(){
+		return adao.selectCardAll();
+	}
+	public List<AccountingDTO> searchCard(String keyword){
+		return adao.searchCard(keyword);
+	}
+	public int insertCard( AccountingDTO dto){
+		return adao.insertCard(dto);
+	}
+	public int deleteCard(String id){
+		return adao.deleteCard(id);
+	}
+	public int updateCard(AccountingDTO dto){
+		return adao.updateCard(dto);
 	}
 }
