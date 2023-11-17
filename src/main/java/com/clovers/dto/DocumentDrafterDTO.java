6b791fb49b19855582243b8a96f99b7ead2f3453
qwee0328 <qwee0328@naver.com>
2023-11-17 17:47:51 +0900

    KDT2PROJ-39 휴가 신청 변경

--- src/main/java/com/clovers/dto/DocumentDrafterDTO.java
+++ src/main/java/com/clovers/dto/DocumentDrafterDTO.java
@@ -1,10 +1,8 @@
 package com.clovers.dto;
 
 import java.sql.Timestamp;
-import java.util.List;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,15 +12,9 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @ToString
-public class VacationDocumentDTO {
+public class DocumentDrafterDTO {
 	private int id;
-	private String document_type_id;
+	private String document_id;
 	private String emp_id;
-	private int save_period;
-	private String security_grade;
-	private Timestamp report_date;
-	private Timestamp approval_date;
-	private Timestamp edit_date;
 }
