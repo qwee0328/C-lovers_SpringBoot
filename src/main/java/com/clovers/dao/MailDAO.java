c438d13eaa98ace0bff656cb7efcd325e0ec8e09
qwee0328 <qwee0328@naver.com>
2023-11-22 15:42:51 +0900

    메일 휴지통에 있는 정보 30일 뒤에 영구 삭제 기능-> 파일도 제거하는 내용 추가해야할듯

--- src/main/java/com/clovers/dao/MailDAO.java
+++ src/main/java/com/clovers/dao/MailDAO.java
@@ -14,102 +14,102 @@ import com.clovers.dto.MemberDTO;
 @Repository
 public class MailDAO {
 	// 메일 DAO
-	
+
 	@Autowired
 	private SqlSession db;
-	
+
 	public int submitSend(EmailDTO dto) {
 		db.insert("Mail.submitSend", dto);
 		return dto.getId();
 	}
-	
+
 	public int submitTempSend(EmailDTO dto) {
 		return db.update("Mail.submitTempSend", dto);
 	}
-	
+
 	public int submitFile(EmailFileDTO dto) {
 		return db.insert("Mail.submitFile", dto);
 	}
-	
+
 	public List<EmailDTO> inBoxList(Map<String, Object> param) {
 		return db.selectList("Mail.inBoxList", param);
 	}
-	
+
 	public int inBoxTotalCount(String receive_id) {
 		return db.selectOne("Mail.inBoxTotalCount", receive_id);
 	}
-	
+
 	public List<EmailDTO> sentBoxList(Map<String, Object> param) {
 		return db.selectList("Mail.sentBoxList", param);
 	}
-	
+
 	public int sentBoxTotalCount(Map<String, Object> param) {
 		return db.selectOne("Mail.sentBoxTotalCount", param);
 	}
-	
+
 	public List<EmailDTO> outBoxList(Map<String, Object> param) {
 		return db.selectList("Mail.outBoxList", param);
 	}
-	
+
 	public int outBoxTotalCount(String send_id) {
 		return db.selectOne("Mail.outBoxTotalCount", send_id);
 	}
-	
+
 	public List<EmailDTO> trashList(Map<String, Object> param) {
 		return db.selectList("Mail.trashList", param);
 	}
-	
+
 	public int trashTotalCount(String id) {
 		return db.selectOne("Mail.trashTotalCount", id);
 	}
-	
+
 	public boolean selectFileByEmailId(int email_id) {
 		Object result = db.selectOne("Mail.selectFileByEmailId", email_id);
 		boolean exist;
-		if(result != null) {
+		if (result != null) {
 			exist = true;
 		} else {
 			exist = false;
 		}
 		return exist;
 	}
-	
+
 	public int deleteMail(int id) {
 		return db.update("Mail.deleteMail", id);
 	}
-	
+
 	public int perDeleteMail(int id) {
 		return db.delete("Mail.perDeleteMail", id);
 	}
-	
+
 	public int restoreMail(int id) {
 		return db.update("Mail.restoreMail", id);
 	}
-	
+
 	public EmailDTO selectAllById(int id) {
 		return db.selectOne("Mail.selectAllById", id);
 	}
-	
+
 	public List<EmailFileDTO> selectAllFileById(int email_id) {
 		return db.selectList("Mail.selectAllFileById", email_id);
 	}
-	
+
 	public int deleteFiles(String sys_name) {
 		return db.delete("Mail.deleteBySysname", sys_name);
 	}
-	
+
 	public List<EmailDTO> selectAllReservationDate() {
 		return db.selectList("Mail.selectAllReservationDate");
 	}
-	
+
 	public int submitReservationMail(Map<String, Object> param) {
 		return db.update("Mail.submitReservationMail", param);
 	}
-	
+
 	public int confirmation(int id) {
 		return db.update("Mail.confirmation", id);
 	}
-	
+
 	public List<Map<String, String>> autoComplete(String keyword) {
 		return db.selectList("Mail.autoComplete", keyword);
 	}
@@ -117,7 +117,7 @@ public class MailDAO {
 	public String getEmailByLoginID(String loginID) {
 		return db.selectOne("Mail.getEmailByLoginID", loginID);
 	}
-	
+
 	public int updateMail(EmailDTO dto) {
 		return db.update("Mail.updateMail", dto);
 	}
@@ -129,5 +129,10 @@ public class MailDAO {
 	// 로그인한 사용자의 이메일 가져오기
 	public String getUserEmail(String loginID) {
 		return db.selectOne("Mail.getUserEmail", loginID);
+	}
+
+	// 휴지통에서 30일 경과한 캘린더 삭제
+	public void autoDeleteInTrash() {
+		db.delete("Mail.autoDeleteInTrash");
 	}
 }
